edbb6a1e21cd7fbee28b300712ee093222b27fe3
Matthew Swift <swiftm@j7c5219349.traveljigsaw.local>
2024-07-30 10:11:51 +0100

    test simple api call to OMDB with console response

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,6 +1,5 @@
-fetch('http://www.omdbapi.com?i=tt3896198&apikey=7eaca78c')
+fetch('https://www.omdbapi.com/?i=tt3896198&apikey=7eaca78c')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error('Error:', error));
 
-  
